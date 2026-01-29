85ec94f1284ff7ae421ef23e4d483efaeee02ec7
JMabee <jmabee@weldqc.com>
2026-01-29 06:14:53 -0500

    Inital load

---
+++ GItTEST/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GItTEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("TEST");
+            Console.ReadLine();
+
+        }
+    }
+}
